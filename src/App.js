6118aeeb3cd0393ea460f5fd52808b5ce251824b
leroy-netizen <leroythomas051@gmail.com>
2022-07-08 15:50:58 +0300

    components| jsx | events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  // declare functions to handle events
+  const handleClick = () => {
+    console.log("I have been Clicked");
+  };
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+  let user = "Ombiji";
+
+  // JSX (Javascript XML) syntax similar to html
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" placeholder="Enter Name" onChange={handleChange} />
+      <h1>Niajeni majamaa</h1>
+      <h1>Moringa School Devs</h1>
+      <button onClick={handleClick}> Click Me</button>
+    </>
   );
 }
 
 export default App;
+
+// Vanilla Javascript Imparative
+// const btn = document.getElementById('buttonn');
+// btn.addEventListener('click', ()=> handleClick)
