57eb5ce395744c5e79912cdb2f73e7680afdc3e9
leroy-netizen <leroythomas051@gmail.com>
2022-07-08 17:44:49 +0300

    state in functional components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import React, { Component } from "react";
-class App extends Component {
-  state = {
-    name: "Baba wa keja",
-    age: 49,
-    school: "Moringa",
-    count: 0,
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Leroy");
+  // const count = useState(0);
+  // console.log(count);
+  // state = {
+  //   name: "Baba wa keja",
+  //   age: 49,
+  //   school: "Moringa",
+  //   count: 0,
+  // };
+
+  const Add = () => {
+    // this.setState({
+    //   count: this.state.count + 1,
+    //   school: (this.state.school = "Technical University Of Kenya"),
+    // });
+    setCount(count + 1);
+  };
+  const Sub = () => {
+    setCount(count - 1);
+  };
+  const handleChangeName = () => {
+    setName("Ombiji");
   };
 
-  Add = () => {
-    this.setState({
-      count: this.state.count + 1,
-      school: (this.state.school = "Technical University Of Kenya"),
-    });
-  };
-  Sub = () => {
-    this.setState({
-      count: this.state.count - 1,
-    });
-  };
-  render() {
-    return (
-      <>
-        <h1>REACT STATE</h1>
-        <h2>Name:{this.state.name}</h2>
-        <h2>Age:{this.state.age}</h2>
-        <h2>School:{this.state.school}</h2>
-        <h4>Counter value:{this.state.count}</h4>
-        <button onClick={this.Add}>Add One</button>
-        <button onClick={this.Sub}>Sub One</button>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1>REACT STATE</h1>
+
+      <h4>Counter value:{count}</h4>
+      <h4>Name:{name}</h4>
+      <button onClick={Add}>Add One</button>
+      <button onClick={Sub}>Sub One</button>
+      <button onClick={handleChangeName}>change Name</button>
+    </>
+  );
+};
 export default App;
